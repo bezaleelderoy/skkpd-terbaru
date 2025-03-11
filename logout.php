a4a1c41c9cb6b312fd80e86e31035e90cfb0ef6c
arie wira kusuma baru <ariewira48@gmail.com>
2025-03-11 19:41:50 +0800

    logout semua session

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@ setcookie('nama_lengkap', '', time(), '/');
 setcookie('nis', '', time(), '/');
 setcookie('angkatan', '', time(), '/');
 setcookie('status', '', time(), '/');
+session_start();
 session_destroy();
 echo "<script>alert('Berhasil Logout');window.location.href='login.php'</script>";
 ?>
